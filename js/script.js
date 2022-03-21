bf2cdd45e1ff1219532ba0379bcc5a39d05147f3
Sheridan Meseck <sheridanmeseck@gmail.com>
2022-03-21 17:15:43 -0500

    adding to code

--- js/script.js
+++ js/script.js
@@ -14,10 +14,16 @@ let pokemonList = [
         height: 59,
         type: ['dark', 'fairy']
     } 
-]
+];
 
+for (let i = 0; i < pokemonList.height; i++){
 
+    if (pokemonList[i].height <68) && pokemonList[i].height >59){
+        console.log(pokemonList[i].height + ' (height:');
+    } else if (pokemonList[i].height)
+}
 
+document.write(pokemonList);
 
 
 
