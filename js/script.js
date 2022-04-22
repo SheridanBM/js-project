5d990029d93784e50d0dfe1075de2343f61aeb72
Sheridan Meseck <sheridanmeseck@gmail.com>
2022-04-22 13:14:43 -0500

    trying to attach css

--- js/script.js
+++ js/script.js
@@ -41,12 +41,13 @@ let pokemonRepository = (function () {
     button.classList.add(".button-class");
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
-    button.addEventListener('click', function (pokemon) {
+  }
+    /*button.addEventListener('click', function (pokemon) {
         console.log(pokemon);
     }
-  function showDetails(pokemon){
+  /*function showDetails(pokemon){
       console.log(pokemonRepository.getAll());
-  }
+  }*/
     return {
       add: add,
       getAll: getAll,
